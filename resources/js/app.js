34389e80ebfaed150ad337cc464970050dd5c5e3
mohammadRasoul <mohammadrasoulasghari1@gmail.com>
2023-09-05 19:01:46 +0330

    Start of the backend section

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import './bootstrap';
+import '../css/bootstrap.min.css';
+import '../css/owl.carousel.min.css';
+import '../css/animate.min.css';
+import '../css/default.css';
+import '../css/style.css';
+import '../css/responsive.css';
+import '../css/fontiran.css';
+import './jquery.min';
+import './popper.min';
+import './bootstrap.min';
+import './owl.carousel.min';
+import './main.js';
+
+
