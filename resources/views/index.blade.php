e6f94b8fbe75729476f357a9b1794ad35c9e2b17
mohammadRasoul <mohammadrasoulasghari1@gmail.com>
2023-09-05 19:25:16 +0330

    add blog page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,29 +24,9 @@
 
     <!--====== PRELOADER PART ENDS ======-->
         
-        <!-- Navbar Area -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="index.html"><img src="img/logo-white.png" alt="logo"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="toggler-icon"></span>
-                    <span class="toggler-icon"></span>
-                    <span class="toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="#home">خانه</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#demo">محصولات</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#features">ارتباظ با ما</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">پر فروش ترین ها</a></li>
-                    </ul>
-
-                    
-                </div>
-            </div>
-        </nav>
-        <!-- End Navbar -->
+       <x-navbar>
+                 
+       </x-navbar>
 
         <!-- Main Banner -->
         <div id="home" class="main-banner bg_cover" style="background-image: url(./img/slider-img.jpg);">
